2b6a84183ff84d71899239bd2432f2f1b06cc8cc
Jennifer <eler@Jennifers-MacBook-Pro.local>
2018-01-24 11:13:04 -0600

    dapper assignmnts etc

---
+++ C#/restauranter/Models/restauranterContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+ 
+namespace restauranter.Models
+{
+    public class restauranterContext : DbContext
+    {
+        // base() calls the parent class' constructor passing the "options" parameter along
+        public restauranterContext(DbContextOptions<restauranterContext> options) : base(options) { }
+    }
+}
