53f443ae98c6448f6ba684a738aa7c8956c145cf
Jennifer <eler@Jennifers-MacBook-Pro.local>
2018-02-15 11:43:06 -0600

    login_reg_complete

--- MEAN/Mongoose/Advanced_Mongoose/login_registration/server.js
+++ MEAN/Mongoose/Advanced_Mongoose/login_registration/server.js
@@ -11,8 +11,8 @@ app.use(express.static(__dirname + "/static"));
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
-bcrypt = require('bcrypt')
-SALT_WORK_FACTOR = 10
+bcrypt = require('bcrypt-as-promised')
+
 
 mongoose.connect('mongodb://localhost/login_registration');
 
@@ -21,64 +21,40 @@ function toLower(v) {
 }
 
 var UserSchema = new mongoose.Schema({
-    email: {type: String, set: toLower, required: true, index: {unique: true}, 
-    validate: {
-        validator: function(value) {
-            return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test( value );
-        },
-        message: "Invalid Email Address!"
-    }
+    email: {
+        type: String, set: toLower, required: true, index: { unique: true },
+        validate: {
+            validator: function (value) {
+                return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(value);
+            },
+            message: "Invalid Email Address!"
+        }
+    },
+    first_name: { type: String, required: [true, "You need to enter a  first name!"], minlength: [2, "first_name must be longer than 2 characters!"] },
+    last_name: { type: String, required: [true, "You need to enter a last name!"], minlength: [2, "last_name must be longer than 2 characters!"] },
+    password: {
+        type: String, required: [true, "You need to enter a password!"], minlength: [8, "Password must be longer than 8 characters"], maxlength: 32,
+        validate: {
+            validator: function (value) {
+                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,32}/.test(value);
+            },
+            message: "Password failed validation, you must have at least 1 number, uppercase and special character"
+        }
+    },
+    birthday: { type: Date, required: true },
 },
-    first_name: {type: String, required: [true, "You need to enter a  first name!"],  minlength: [2, "first_name must be longer than 2 characters!"]},
-    last_name: {type: String, required: [true, "You need to enter a last name!"],  minlength: [2, "last_name must be longer than 2 characters!"]},
-    password: {type: String, required: [true, "You need to enter a password!"], minlength: [8, "Password must be longer than 8 characters"],maxlength: 32,  
-    validate: {
-        validator: function( value ) {
-          return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,32}/.test( value );
-        },
-        message: "Password failed validation, you must have at least 1 number, uppercase and special character"
-      }
-},
-    birthday: {type: Date, required: true},
-},
-    { timestamps: true});
-    // UserSchema.path('email').validate(function (email) {
-    //     var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    //     return emailRegex.test(email.text); // Assuming email has a text attribute
-    //  }, 'The e-mail field cannot be empty.')
-     
-    
-UserSchema.pre('save', function(next) {
-    var user = this;
+    { timestamps: true });
 
-    // only hash the password if it has been modified (or is new)
-    if (!user.isModified('password')) return next();
-
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if (err) return next(err);
-
-        // hash the password along with our new salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) return next(err);
-
-            // override the cleartext password with the hashed one
-            user.password = hash;
+UserSchema.pre('save', function (next) {
+    bcrypt.hash(this.password, 10)
+        .then(function (hashed_pw) {
+            this.password = hashed_pw;
             next();
+        })
+        .catch(function (errors) {
+            console.log(errors)
         });
-    });
 });
-UserSchema.post('save', function(user) {
-    var user = this;
-    req.sesison.id = this.id;
-    console.log(this.id);
-})
-        // UserSchema.methods.comparePassword = function(password_confirmation, cb) {
-        //     bcrypt.compare(password_confirmation, this.password, function(err, isMatch) {
-        //         if (err) return cb(err);
-        //         cb(null, isMatch);
-        //     });
-        // };
 
 mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
 var User = mongoose.model('User')
@@ -92,50 +68,73 @@ app.set('views', __dirname + '/views');
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render("index");
-   })
- 
-app.post('/register_user', function(req, res) {
-    console.log(req.body.email);
-    console.log(req.body.password)
-    console.log(req.body.password_confirm)
-    if(req.body.password == req.body.password_confirm)
-    {
-        var user = new User 
-        ({
-            email: req.body.email,
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            password: req.body.password,
-            birthday: req.body.birthday
-    
-        })
-        user.save(function(err){
-            if(err) {
-                console.log("Something went wrong at line 96!")
-                res.render('index', {errors: [err]})
+})
+
+app.post('/register_user', function (req, res) {
+    if (req.body.password == req.body.password_confirm) {
+        var user = new User
+            ({
+                email: req.body.email,
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                password: req.body.password,
+                birthday: req.body.birthday
+            })
+        user.save(function (err) {
+            if (err) {
+                res.render('index', { errors: [err] })
             }
             else {
                 console.log("successfully added a user!")
-                console.log("do you exist?", user.id)
-                // req.session.id = user.id
+                req.session.user_id = user.id;
                 res.redirect('/success');
             }
         });
     }
-    else
-    {
+    else {
         console.log("password confirmation not met!")
         res.redirect('/')
     }
 });
 
-app.get('/success', function(req, res) {
-    res.render("success");
+app.post('/login_user', function (req, res) {
+    User.findOne({ email: req.body.email_login }, function (err, user) {
+        if (err) {
+            res.render('index', { errors: [err] })
+        }
+        if (!user) {
+            res.render('index', { errors: [err] })
+        }
+        if (user) 
+        {
+            console.log(user)
+            bcrypt.compare(req.body.password_login, user.password)
+                .then(function (logged_in) {
+                    req.session.user_id = user.id;
+                    res.redirect('/success')
+                })
+                .catch(function (error) {
+                    res.redirect('/');
+                })
+        }
+    })
+});
+
+app.get('/success', function (req, res) {
+    if (req.session.user_id === undefined) {
+        res.redirect('/');
+    }
+    else {
+        res.render("success");
+    }
 })
-   
-app.listen(8000, function() 
-{
+
+app.get('/logout', function (req, res) {
+    req.session.destroy();
+    res.redirect('/');
+})
+app.listen(8000, function () {
     console.log("listening on port 8000");
 })
