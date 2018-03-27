59ea4176a8a453795b62690a0e7ad481cd7e698e
Jennifer <eler@Jennifers-MacBook-Pro.local>
2018-03-27 15:42:14 -0500

    boardgame marketplacedeleted

--- Web-Fun/Javascript/randomchance.js
+++ Web-Fun/Javascript/randomchance.js
@@ -4,8 +4,10 @@ function randomChance(quarters) {
         var slotResult = Math.floor(Math.random() * 100) + 1; //chance of winning
         var winnings = Math.floor(Math.random()*50) + 50 //amount of winnings
         var winningNumber = Math.floor(Math.random()*100) + 1 //winning number
+        console.log(winningNumber)
         if(slotResult === winningNumber) {
             quarters+=winnings;
+            console.log("yay winning!")
             return quarters;
         }
         else {
@@ -16,3 +18,19 @@ function randomChance(quarters) {
     }
 }
 console.log(randomChance(1500));
+
+function randomChance2(quarters)
+{
+    for(var i = quarters; i>0; i--)
+    {
+        var number = Math.floor(Math.random()*100) +1
+        if(number === 100)
+        {
+            console.log("winning")
+            return quarters;
+        }
+    }
+    return "NOPE"
+}
+
+randomChance2(100)
